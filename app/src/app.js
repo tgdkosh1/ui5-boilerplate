f238345e48e93dab86a0af6d312bc1c658869c4d
Hirschfeld Gerhard, ENT-SAP-PSR-MBL-MII <Gerhard.Hirschfeld@swisscom.com>
2014-08-14 15:26:10 +0200

    update

--- app/src/app.js
+++ app/src/app.js
@@ -1,3 +1,11 @@
 /**
  * Created by tzhhige1 on 14/08/14.
  */
+
+
+sap.ui.localResources( "view" );
+sap.ui.localResources( "util" );
+
+// Launch application
+jQuery.sap.require( "Application" );
+var oApp = new Application( {root: "content"} );
