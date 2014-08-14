b1117ce95b7743faa3ab7f4f754bd1442c184013
Robert Kleger <robert.kleger@swisscom.com>
2014-08-14 15:53:22 +0200

    Bugfix

--- app/src/app.js
+++ app/src/app.js
@@ -13,8 +13,6 @@ var oApp = new Application( {root: "content"} );
 jQuery.sap.declare( 'Application' );
 jQuery.sap.require( 'sap.ui.app.Application' );
 
-var oApp = new Application( {root: "content"} );
-
 sap.ui.app.Application.extend( 'Application', {
     init: function(){
 
