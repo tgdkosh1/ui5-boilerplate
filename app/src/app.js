eb6f3789fd4a7525b3cb3fe238f27c50b8ed86ba
Hirschfeld Gerhard, ENT-SAP-PSR-MBL-MII <Gerhard.Hirschfeld@swisscom.com>
2014-08-14 15:29:28 +0200

    update

--- app/src/app.js
+++ app/src/app.js
@@ -7,5 +7,17 @@ sap.ui.localResources( "view" );
 sap.ui.localResources( "util" );
 
 // Launch application
-jQuery.sap.require( "Application" );
+jQuery.sap.declare( 'Application' );
+jQuery.sap.require( 'sap.ui.app.Application' );
+
+sap.ui.app.Application.extend( 'Application', {
+    init: function(){
+
+    },
+    main: function(){
+
+    }
+} );
+
+
 var oApp = new Application( {root: "content"} );
